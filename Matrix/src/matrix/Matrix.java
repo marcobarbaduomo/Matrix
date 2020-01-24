e6b651bd8f1b0033444f15eda2f42ed2de5a089d
barbaduomo.marco <barbaduomo.marco@BIBLIO-PC04.OSDBSESTO.local>
2020-01-24 12:03:18 +0100

    Matrix 3x3 start

--- Matrix/src/matrix/Matrix.java
+++ Matrix/src/matrix/Matrix.java
@@ -1,39 +1,35 @@
 package matrix;
 
-import java.io.File;
+//import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
 
 public class Matrix {
 
     public static void main(String[] args) throws FileNotFoundException {
-        
+
         Scanner input = new Scanner(System.in); //matrix.txt
-        
+
         int rows = 3;
         int columns = 3;
-        
+
         System.out.println("matrix has " + rows + " rows and " + columns + " columns");
         int[][] a = new int[rows][columns];
-        
-        for (int b=0; b<rows; b++) {
-            for (int c=0; c<columns; c++) {
-                System.out.print("Inserire numero:");
+
+        for (int b = 0; b < rows; b++) {
+            for (int c = 0; c < columns; c++) {
+                System.out.print("Inserire valore di riga " + (b + 1) + " e colonna " + (c + 1) + ":");
                 a[b][c] = input.nextInt();
             }
         }
-        
+
         System.out.println("");
-        
-        for (int i = 0; i <rows; i++) {
-            for (int j = 0; j <columns; j++) {
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
                 System.out.print(a[i][j] + " ");
             }
             System.out.print("\n");
-        } //dasdads
+        }
     }
-        
-    
-    
 }
