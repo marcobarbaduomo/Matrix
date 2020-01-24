2a05d19326a08afa0ca156e3372ff493a97b0745
barbaduomo.marco <barbaduomo.marco@BIBLIO-PC04.OSDBSESTO.local>
2020-01-24 11:51:14 +0100

    Start.2

--- Matrix/src/matrix/Matrix.java
+++ Matrix/src/matrix/Matrix.java
@@ -1,9 +1,39 @@
 package matrix;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
 public class Matrix {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         
+        Scanner input = new Scanner(System.in); //matrix.txt
+        
+        int rows = 3;
+        int columns = 3;
+        
+        System.out.println("matrix has " + rows + " rows and " + columns + " columns");
+        int[][] a = new int[rows][columns];
+        
+        for (int b=0; b<rows; b++) {
+            for (int c=0; c<columns; c++) {
+                System.out.print("Inserire numero:");
+                a[b][c] = input.nextInt();
+            }
+        }
+        
+        System.out.println("");
+        
+        for (int i = 0; i <rows; i++) {
+            for (int j = 0; j <columns; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
     }
+        
+    
     
 }
